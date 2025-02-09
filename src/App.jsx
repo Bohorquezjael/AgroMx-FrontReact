b76c98741892d0298bb3bfb2bacfa8a933bd2c3b
Jael Bohorquez <Jaelbkz@icloud.com>
2025-02-09 14:05:49 -0600

    added components base

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import Footer from './components/footer/Footer.jsx '
-import Header from './components/header/Header.jsx '
+import Footer from './components/footer/footer'
+import Header from './components/header/header'
 
 function App() {
 
